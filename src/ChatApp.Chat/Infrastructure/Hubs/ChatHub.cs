257ca16877f0b3fd90fa826a158643e3b31bf899
F1elle <vanya030680@gmail.com>
2025-11-14 03:51:56 +0100

    hub in progress

--- src/ChatApp.Chat/Infrastructure/Hubs/ChatHub.cs
+++ src/ChatApp.Chat/Infrastructure/Hubs/ChatHub.cs
@@ -1,6 +1,8 @@
 using ChatApp.Chat.Domain;
+using ChatApp.Chat.Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using StackExchange.Redis;
 
 namespace ChatApp.Chat.Infrastructure.Hubs;
 
@@ -16,16 +18,55 @@ public interface IChatClient
 [Authorize]
 public class ChatHub : Hub<IChatClient>
 {
-    public async Task SendMessage(Message message) { }
+    private readonly ChatDbContext _dbContext;
+    private readonly IConnectionMultiplexer _redis;
+    // redis
+    private readonly ILogger<ChatHub> _logger;
+
+    public ChatHub(
+        ChatDbContext dbContext,
+        IConnectionMultiplexer redis, 
+        ILogger<ChatHub> logger)
+    {
+        _dbContext = dbContext;
+        _redis = redis;
+        _logger = logger;
+    }
 
     public override async Task OnConnectedAsync()
     {
+        var userId = GetUserId();
+
+        await Groups.AddToGroupAsync(Context.ConnectionId, $"user:{userId}");
+
+        _logger.LogInformation("User {UserId} connected", userId);
+
+        await base.OnConnectedAsync();
+    }
+
+    public async Task JoinChat(Guid chatId)
+    {
+        var userId = GetUserId();
+        var redis = _redis.GetDatabase();
+
+        // var isParticipant = await _dbContext.
+    }
+
+    public async Task SendMessage(Message message)
+    {
 
     }
+    
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
         await base.OnDisconnectedAsync(exception);
     }
+    
 
+    private Guid GetUserId()
+    {
+        var claim = (Context.User?.FindFirst("sub")?.Value) ?? throw new HubException("Not authenticated");
+        return Guid.Parse(claim);
+    }
 }

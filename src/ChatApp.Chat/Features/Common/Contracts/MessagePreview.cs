6e57255293bd4096acc3d95b0e21891646a284c6
F1elle <vanya030680@gmail.com>
2026-09-20 21:52:36 +0200

    todo in messagepreview

--- src/ChatApp.Chat/Features/Common/Contracts/MessagePreview.cs
+++ src/ChatApp.Chat/Features/Common/Contracts/MessagePreview.cs
@@ -3,6 +3,7 @@ using ChatApp.Chat.Domain.Enums;
 namespace ChatApp.Chat.Features.Common.Contracts;
 
 // Used when message preview is needed - notification, chat entry, etc.
+// TODO: make this specific to chat list, use different for notifications
 public sealed record MessagePreview(
     string SenderName,
     Guid ChatId,
